49829d8ca75ffe2d07a8b534d58063393c853cda
@FernandaQAMottu <fernanda.gomes@mottu.com.br>
2023-11-21 20:58:22 -0300

    Criação de todos os testes web do site toDo Mvc

---
+++ src/config/test.js
@@ -0,0 +1,8 @@
+const base = require ('@playwright/test')
+
+exports.test = base.test.extend({
+    data: async ({page}, use) => {
+        const data = require('./data')
+        await use(data)
+    }
+})
