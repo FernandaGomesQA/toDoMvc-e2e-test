ef431f234eae68a4561ab6af4be981c38d6df9b9
FernandaGomesQA <127121233+FernandaGomesQA@users.noreply.github.com>
2023-11-26 20:44:45 -0300

    Remoção de comentários

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,46 +14,38 @@ class HomePage extends Page {
         this.btnClearCompleted = this.locator('button[class="clear-completed"]')
     }
 
-    //validar o titulo da pagina
     async validateTitle(title){
         await expect(this.pageTitle).toHaveText(title)
     }
 
-    //inserir uma tarefa
     async insertTask(task) {
         await this.inputToDo.fill(task)
         await sleep(3)
         await this.inputToDo.press('Enter')
     }
 
-    //validar a tarefa inserida
     async validateInsertTask(task) {
         await this.btnAll.click()
         await sleep(5)
         await expect(this.taskAdd).toHaveText(task)
     }
 
-    //clicar em completar a tarefa
     async taskCompleted(){
         await this.btnAll.click()
         await this.checkbox.click()
     }
 
-    //validar a tarefa na aba completed
     async validateTaskCompleted(task){
         await this.btnCompleted.click()
         await sleep(2)
         await expect(this.taskAdd).toHaveText(task)
     }
     
-    //limpar a lista
     async clearTaskList() {
         await this.btnClearCompleted.click()
         await sleep(2)
         await expect(this.taskAdd).toBeHidden()
     }
-
-    
 }
 
 module.exports = {HomePage}
